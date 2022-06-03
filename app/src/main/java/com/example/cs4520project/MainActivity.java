443d3ea769e9fc63c9b8ea46b203782865768eb1
Dima Vremekno <dimavrem22@gmail.com>
2022-06-02 21:28:28 -0400

    calander commit

--- app/src/main/java/com/example/cs4520project/MainActivity.java
+++ app/src/main/java/com/example/cs4520project/MainActivity.java
@@ -1,23 +1,71 @@
 package com.example.cs4520project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
-    ConstraintLayout fragmentContainer;
+public class MainActivity extends AppCompatActivity implements CalendarView.OnDateChangeListener,
+        View.OnClickListener {
+
+    private final long DAY_IN_MS = 1000 * 60 * 60 * 24;
+
+    int day, month, year;
+
+    private TextView dateText, monthText;
+
+    private ConstraintLayout fragmentContainer;
+    private ImageView calendarButton, nextArrow, prevArrow;
+    private CalendarView calender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.calender = this.findViewById(R.id.calendarView);
+        this.calender.setMaxDate(System.currentTimeMillis());
+        this.calender.setOnDateChangeListener(this);
+        this.calender.setVisibility(View.INVISIBLE);
+        this.calender.setEnabled(false);
 
-        this.fragmentContainer = this.findViewById(R.id.fragment_container);
 
-        this.getSupportFragmentManager().beginTransaction().add(R.id.fragment_container,
+
+        this.fragmentContainer = this.findViewById(R.id.fragment_layout);
+
+        this.dateText = this.findViewById(R.id.text_date);
+        this.monthText = this.findViewById(R.id.text_month);
+
+        java.util.Date date= new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+
+        this.dateText.setText(cal.get(Calendar.DAY_OF_MONTH) + "");
+        this.monthText.setText(new Utils().monthString(cal.get(Calendar.MONTH))+ "");
+
+
+        this.nextArrow = this.findViewById(R.id.next_button);
+        this.nextArrow.setOnClickListener(this);
+        this.prevArrow = this.findViewById(R.id.prev_button);
+        this.prevArrow.setOnClickListener(this);
+
+        this.calendarButton = this.findViewById(R.id.cal_button);
+        this.calendarButton.setOnClickListener(this);
+
+
+
+        this.getSupportFragmentManager().beginTransaction().add(R.id.main_layout,
                 new LogInFragment(), "login fragment").addToBackStack(null).commit();
 
 
@@ -26,4 +74,50 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onSelectedDayChange(@NonNull CalendarView view,
+                                    int year, int month, int dayOfMonth) {
+        this.calender.setVisibility(View.INVISIBLE);
+        this.calender.setEnabled(false);
+
+        Calendar cal = Calendar.getInstance();
+
+        cal.set(year, month, dayOfMonth);
+        this.calender.setDate(cal.getTimeInMillis());
+
+            this.DateSelected(year, month, dayOfMonth);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("ass", calender.getDate()+"");
+        if(v.getId() == this.calendarButton.getId()){
+            this.calender.setVisibility(View.VISIBLE);
+            this.calender.setEnabled(true);
+        }
+        else if (v.getId() == this.prevArrow.getId()){
+            this.calender.setDate(calender.getDate() - DAY_IN_MS);
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(calender.getDate());
+            this.dateText.setText(cal.get(Calendar.DAY_OF_MONTH) + "");
+            this.monthText.setText(new Utils().monthString(cal.get(Calendar.MONTH))+ "");
+            Log.d("ass", calender.getDate()+"");
+
+        }
+        else if (v.getId() == this.nextArrow.getId()) {
+            this.calender.setDate(calender.getDate() + DAY_IN_MS);
+            Calendar cal = Calendar.getInstance();
+            cal.setTimeInMillis(calender.getDate());
+            this.dateText.setText(cal.get(Calendar.DAY_OF_MONTH) + "");
+            this.monthText.setText(new Utils().monthString(cal.get(Calendar.MONTH))+ "");
+            Log.d("ass", calender.getDate()+"");
+        }
+    }
+
+    private void DateSelected(int year, int month, int dayOfMonth){
+        this.dateText.setText(dayOfMonth + "");
+        this.monthText.setText(new Utils().monthString(month) + "");
+
+    }
 }
---
+++ app/src/main/java/com/example/cs4520project/Utils.java
@@ -0,0 +1,36 @@
+package com.example.cs4520project;
+
+public class Utils {
+
+    public String monthString(int month) {
+        switch (month) {
+            case 0:
+                return "January";
+            case 1:
+                return "February";
+            case 2:
+                return "March";
+            case 3:
+                return "April";
+            case 4:
+                return "May";
+            case 5:
+                return "June";
+            case 6:
+                return "July";
+            case 7:
+                return "August";
+            case 8:
+                return "September";
+            case 9:
+                return "October";
+            case 10:
+                return "November";
+            case 11:
+                return "December";
+            default:
+                return "Invalid month";
+        }
+    }
+
+}
