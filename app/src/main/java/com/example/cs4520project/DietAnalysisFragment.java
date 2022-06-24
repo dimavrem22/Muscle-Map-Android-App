d0d20558ff24e1efd47f88293367fdc53876ce65
Ryan Keleti <github@ryankeleti.com>
2022-06-24 14:53:06 -0400

    Work on diet analysis

--- app/src/main/java/com/example/cs4520project/DietAnalysisFragment.java
+++ app/src/main/java/com/example/cs4520project/DietAnalysisFragment.java
@@ -7,18 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.LineDataSet;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 
 public class DietAnalysisFragment extends Fragment {
     public static final String FRAGMENT_TAG = "DIET_ANALYSIS_FRAGMENT";
     private static final String ARG_MEALS = "MEALS";
+    private static final Calendar calendar = Calendar.getInstance();
 
     private List<Meal> meals;
 
@@ -58,7 +64,68 @@ public class DietAnalysisFragment extends Fragment {
         Spinner chartSelect = rootView.findViewById(R.id.dietChartSelect);
         LineChart chart = rootView.findViewById(R.id.dietAnalysisChart);
 
+        chartSelect.setAdapter(new ArrayAdapter<>(rootView.getContext(),
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                ChartType.values()));
+        chartSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ChartType chartType = (ChartType) parent.getItemAtPosition(position);
+                switch (chartType) {
+                    case CALORIES_PAST_WEEK:
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        TextView averageBreakfastCalories = rootView.findViewById(R.id.textViewAvgBreakfastCalories);
+        averageBreakfastCalories.setText(Double.toString(averageCalories(MealType.BREAKFAST)));
+
+        TextView averageLunchCalories = rootView.findViewById(R.id.textViewAvgLunchCalories);
+        averageLunchCalories.setText(Double.toString(averageCalories(MealType.LUNCH)));
+
+        TextView averageDinnerCalories = rootView.findViewById(R.id.textViewAvgDinnerCalories);
+        averageDinnerCalories.setText(Double.toString(averageCalories(MealType.DINNER)));
+
+        TextView averageSnackCalories = rootView.findViewById(R.id.textViewAvgSnackCalories);
+        averageSnackCalories.setText(Double.toString(averageCalories(MealType.SNACK)));
+
         return rootView;
     }
 
+    private int[] caloriesPastWeek(Date date) {
+        calendar.setTime(date);
+        int[] week = new int[7];
+        for (int i = 0; i < 7; i++) {
+            week[i] = caloriesOnDate(calendar.getTime());
+            calendar.add(Calendar.DATE, -1);
+        }
+        return week;
+    }
+
+    private int caloriesOnDate(Date date) {
+        return meals.stream()
+                .filter(meal -> date.equals(meal.getDate()))
+                .mapToInt(Meal::getCalories)
+                .sum();
+    }
+
+    private double averageCalories(MealType mealType) {
+        return meals.stream()
+                .filter(meal -> meal.getMealType() == mealType)
+                .mapToInt(Meal::getCalories)
+                .average()
+                .orElse(0.0);
+    }
+
+    enum ChartType {
+        CALORIES_PAST_WEEK,
+    }
 }
